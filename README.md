94ae5acf0a003b69b9cb9caa65da3e30cb036872
urielch <uriel.chemouni@gmail.com>
2023-06-05 09:00:13 +0300

    order icons by name

--- README.md
+++ README.md
@@ -15,8 +15,8 @@ For a transparent usage:
   "imports": {
     "preact":  "https://esm.sh/preact@10.15.1",
     "preact/": "https://esm.sh/preact@10.15.1/",
-    "react-icons/vsc":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-vsc@1.0.8/mod.ts",
-    "react-icons/vsc/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-vsc@1.0.8/ico/",
+    "react-icons/vsc":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-vsc@1.0.9/mod.ts",
+    "react-icons/vsc/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-vsc@1.0.9/ico/",
   }
 }
 ```
@@ -24,7 +24,7 @@ For a transparent usage:
 ## Import an icon without import_map by and afer loading all icons from the lib vsc
 
 ```ts
-import { VscAccount } from "https://deno.land/x/react_icons_vsc@1.0.8/mod.ts"
+import { VscAccount } from "https://deno.land/x/react_icons_vsc@1.0.9/mod.ts"
 ```
 
 ## import_map import an icon from all icons
--- mod.ts
+++ mod.ts
@@ -17,8 +17,8 @@
  *   "imports": {
  *     "preact":  "https://esm.sh/preact@10.15.1",
  *     "preact/": "https://esm.sh/preact@10.15.1/",
- *     "react-icons/vsc":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-vsc@1.0.8/mod.ts",
- *     "react-icons/vsc/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-vsc@1.0.8/ico/",
+ *     "react-icons/vsc":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-vsc@1.0.9/mod.ts",
+ *     "react-icons/vsc/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-vsc@1.0.9/ico/",
  *   }
  * }
  * ```
@@ -26,7 +26,7 @@
  * ## Import an icon without import_map by and afer loading all icons from the lib vsc
  * 
  * ```ts
- * import { VscAccount } from "https://deno.land/x/react_icons_vsc@1.0.8/mod.ts"
+ * import { VscAccount } from "https://deno.land/x/react_icons_vsc@1.0.9/mod.ts"
  * ```
  * 
  * ## import_map import an icon from all icons
