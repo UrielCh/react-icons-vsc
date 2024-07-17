78559ca1861a4a27473ed627686d5bd9676b851c
urielch <uriel.chemouni@gmail.com>
2024-07-17 13:35:02 +0200

    order icons by name

--- ico/VscListTree.ts
+++ ico/VscListTree.ts
@@ -1,6 +1,6 @@
 import { GenIcon, type IconBaseProps } from "../deps.ts";
 
 export function VscListTree(props: IconBaseProps) {
-  return GenIcon({tag:"svg",attr:{viewBox:"0 0 16 16",fill:"currentColor"},child:[{tag:"rect",attr:{x:"4",y:"9","width":"9","height":"1"}},{tag:"rect",attr:{x:"4",y:"12","width":"7","height":"1"}},{tag:"rect",attr:{x:"4",y:"6","width":"10","height":"1"}},{tag:"rect",attr:{x:"1",y:"3","width":"11","height":"1"}},{tag:"rect",attr:{x:"4",y:"4","width":"1","height":"9"}}]})(props);
+  return GenIcon({tag:"svg",attr:{viewBox:"0 0 16 16",fill:"currentColor"},child:[{tag:"rect",attr:{x:"4",y:"9","width":"9","height":"1"}},{tag:"rect",attr:{x:"4",y:"12","width":"7","height":"1"},child:[]},{tag:"rect",attr:{x:"4",y:"6","width":"10","height":"1"},child:[]},{tag:"rect",attr:{x:"1",y:"3","width":"11","height":"1"},child:[]},{tag:"rect",attr:{x:"4",y:"4","width":"1","height":"9"},child:[]}]})(props);
 }
 export default VscListTree;
