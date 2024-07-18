f4455efb179d4159b0abd071b64a21249227487a
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:58 +0200

    update doc

--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/vsc)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/vsc
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/vsc
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # VS Code Icons icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/vsc)](https://jsr.io/@preact-icons/vsc)
- * 
- * **License** [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)
- * 
- * **Project** [https://github.com/microsoft/vscode-codicons](https://github.com/microsoft/vscode-codicons)
- * 
- * [See available icons here](https://react-icons.deno.dev/vsc)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/vsc
- * dnpx jsr add @preact-icons/vsc
- * pnpm dlx jsr add @preact-icons/vsc
- * bunx jsr add @preact-icons/vsc
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import VscAccount from "react-icons/vsc/VscAccount.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
